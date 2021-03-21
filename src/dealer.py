de7825d17fd371e80f0e69d3c2829c5c8d88cddf
AaronTheIssueGuy <aaronhafer@gmx.de>
2021-03-21 16:02:52 +0100

    change the way I load and safe data

--- src/dealer.py
+++ src/dealer.py
@@ -8,34 +8,58 @@ class Dealer:
 
         cards_data = helpers.helpers.read_file("../resources/cards.dat")
         self.cards = cards_data
+
         try:
-            cardlist_data = helpers.helpers.read_file("../resources/cardlist.dat")
+            self.savedata = helpers.helpers.read_file("../resources/save.dat")
+
+            self.player = self.savedata["player"]
+            self.table = self.savedata["table"]
+            self.round = self.savedata["round"]
+            self.cardlist = self.savedata["cardlist"]
         except:
-            cardlist_data = None
-        self.cardlist = cardlist_data
+            print("Failed to load savedata. New round was started.")
+            self.newround()
 
-        if self.cardlist == None:
-            self.cardlist = []
-            for color in ["cross","spades","peak","diamonds"]:
-                for card in self.cards[color]:
-                    self.cardlist.append(card)
-
-
+    def move(self):
+        if self.round[1] == player_count - 1: #player 0 to 3 instead of 1 to 4
+            self.round = [self.round[0]+1,0]
+            print("here")
+        else:
+            self.round = [self.round[0],self.round[1]+1]
+        self.save_data()
 
     def newround(self):
-        helpers.helpers.save_data("../resources/player.dat", None)
-        helpers.helpers.save_data("../resources/table.dat", None)
-        helpers.helpers.save_data("../resources/cardlist.dat", None)
+        self.player = []
+        for p in range(player_count):
+            self.player.append([])
+        self.table = [0,[]]
+        self.round = [0,0] # The total rounds played and the information which players turn it is
+        self.cardlist = []
+        for color in ["cross","spades","peak","diamonds"]:
+            for card in self.cards[color]:
+                self.cardlist.append(card)
+        self.save_data()
+
     def dealcards(self,count):
         cards = []
         for num in range(count):
             card = random.choice(self.cardlist)
             cards.append(card)
             self.cardlist.remove(card)
-            print(self.cardlist)
+
+        self.save_data()
+
         return(cards)
+
+
+
     def save_data(self):
-        helpers.helpers.save_data("../resources/cardlist.dat", self.cardlist)
+        value = {
+          "player": self.player,
+          "table": self.table,
+          "round": self.round,
+          "cardlist": self.cardlist
+        }
+        helpers.helpers.save_data("../resources/save.dat", value)
 
 dealer = Dealer()
-dealer.newround()
--- src/table.py
+++ src/table.py
@@ -1,24 +1,45 @@
 import helpers
 import hands
+import dealer
 
 players = 4
 buyin = 5
 
 class Table:
     def __init__(self):
-        try:
-            table_data = helpers.helpers.read_file("../resources/table.dat")
-        except:
-            table_data = None
-        self.table = table_data if table_data != None else [[],["card"]]
+        cards_data = helpers.helpers.read_file("../resources/cards.dat")
+        self.cards = cards_data
+        while True:
+            print("trying to load savedata")
+            try:
+                self.savedata = helpers.helpers.read_file("../resources/save.dat")
+
+                self.player = self.savedata["player"]
+                self.table = self.savedata["table"]
+                self.round = self.savedata["round"]
+                self.cardlist = self.savedata["cardlist"]
+                break
+            except:
+                print("Failed to load savedata. New round was started.")
+                dealer.dealer.newround()
+
         self.pot = self.table[0]
         self.open_cards = self.table[1]
+
     def add_open(self, cardlist):
         for card in cardlist:
             self.open_cards.append(card)
 
+        self.save_data()
+
     def save_data(self):
-        helpers.helpers.save_data("../resources/table.dat", [self.pot,self.open_cards])
+        self.table = [self.pot,self.open_cards]
+        value = {
+          "player": self.player,
+          "table": self.table,
+          "round": self.round,
+          "cardlist": self.cardlist
+        }
+        helpers.helpers.save_data("../resources/save.dat", value)
 
 table = Table()
-table.save_data()
